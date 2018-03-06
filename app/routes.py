33454bf65d3fa78d63c3d3d0939617c54c8fff4c
DGrutt <DG36@buffalo.edu>
2018-03-06 12:18:30 -0500

    fixed PDF uploading and added view for uploaded PDFs.

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,13 @@
 import io
 from flask import Flask, render_template, flash, redirect, url_for, request
-from flask_uploads import UploadSet, configure_uploads, IMAGES 
+from flask_uploads import UploadSet, configure_uploads 
 # add to import line above PDFs and remove IMAGES
 from app import app
 from app.forms import ViewForm
 from PIL import Image
 import pytesseract
 from wand.image import Image as wi
-
+import os
 
 @app.route('/')
 @app.route('/index')
@@ -29,7 +29,7 @@ def index():
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
-    photos = UploadSet('photos', IMAGES)
+    photos = UploadSet('photos', ['pdf'])
     app.config['UPLOADED_PHOTOS_DEST'] = 'app/static/img'
     configure_uploads(app, photos)
     if request.method == 'POST' and 'photo' in request.files:
@@ -37,13 +37,30 @@ def upload():
         return filename 
     return render_template('upload.html')
 
+def make_tree(path):
+    tree = dict(name=path, children=[])
+    try: lst= os.listdir(path)
+    except OSError:
+        pass #ignore errors
+    else:
+        for name in lst:
+            fn = os.path.join(path, name)
+            if os.path.isdir(fn):
+                tree['children'].append(make_tree(fn))
+            else:
+                tree['children'].append(dict(name=fn))
+    return tree     
+
+@app.route('/complete', methods=['GET', 'POST'])
+def complete():
+    path = os.path.expanduser(u'~')
+    #return render_template('complete.html', tree=make_tree("/home/dan/PDFOCR/app/static/img"))
+    return render_template('complete.html', tree=make_tree("app/static/img"))
+
 @app.route('/view', methods=['GET', 'POST'] )
 #Note the code to display a jpeg is still in this function but unused.
 def view():
     form = ViewForm()
-    if form.validate_on_submit():
-        flash('document coded {}'.format(form.relevance.data))
-        return redirect(url_for('index'))
     docimage = '/static/Irregularis_sampletext.png'
     im = Image.open('app/static/Irregularis_sampletext.png')
     text = pytesseract.image_to_string(im, lang = 'eng')
