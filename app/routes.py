f6f8dd5d614b5e667507c57b67470cb4c0474013
DGrutt <DG36@buffalo.edu>
2018-03-25 17:44:58 -0400

    adding NLTK

--- app/routes.py
+++ app/routes.py
@@ -13,6 +13,16 @@ import os
 #from matchFind import matchFind
 from uuid import uuid4
 
+import nltk
+nltk.download('movie_reviews')
+import random
+from nltk.corpus import movie_reviews
+from nltk.classify import *
+import pickle
+#adding nltk
+
+
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -36,7 +46,46 @@ def angular():
 
 @app.route('/sentiment_all', methods=['GET', 'POST'])
 def sentiment_all():
-    return render_template('sentiment_all.html')
+    documents = [(list(movie_reviews.words(fileid)), category)
+                    for category in movie_reviews.categories()
+                    for fileid in movie_reviews.fileids(category)]
+
+    random.shuffle(documents)
+
+    all_words = []
+
+    for w in movie_reviews.words():
+        all_words.append(w.lower())
+
+    all_words = nltk.FreqDist(all_words)
+    
+    word_features = list(all_words.keys()) [:3000]
+    
+    def find_features(document):
+        words = set(document)
+        features = {}
+        for w in word_features:
+            features[w]=(w in words)
+        return features
+    
+    featuresets = [(find_features(rev), category) for (rev, category) in documents]
+
+    training_set = featuresets[:1900]
+    testing_set = featuresets[1900:]
+
+    classifier = nltk.NaiveBayesClassifier.train(training_set)
+    
+    save_classifier = open("naivebayes.pickle", "wb")
+    pickle.dump(classifier, save_classifier)
+    save_classifier.close()    
+
+    classifier_f = open("naivebayes.pickle", "rb")
+    classifier = pickle.load(classifier_f)
+    classifier_f.close()
+    
+    accuracy = nltk.classify.accuracy(classifier, testing_set)*100
+    informativeFeatures = classifier.most_informative_features(15)
+    return render_template('sentiment_all.html', accuracy=accuracy, informativeFeatures=informativeFeatures)
 
 @app.route('/keywordMatches', methods=['GET', 'POST'])
 def keywordMatches():
